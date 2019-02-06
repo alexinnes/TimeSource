9ec65ad1bda267e079fec5201e75a3c6537e746e
Alex Innes <alexnixoninnes@gmail.com>
2019-02-06 13:11:52 +0000

    Initial File

---
+++ timesource.ps1
@@ -0,0 +1,43 @@
+
+
+
+$server = Get-ADComputer -Filter {operatingsystem -like "*server*"}
+
+function Get-TimeSource {
+    [CmdletBinding()]
+    param ([mandi]
+
+        $server
+    )
+
+    begin {
+        $timeObjectArray = @()
+    }
+
+    process {
+        foreach($serv in $server){
+            if(Test-Connection $serv.Name -Count 1 -Quiet){
+                $run = Invoke-Command -ComputerName $serv.Name -ScriptBlock {$time = & 'w32tm' '/query', '/status';$time }
+                $timeObject = New-Object pscustomobject -Property @{
+                    Server= $serv.name
+                    NTP = ($run | Select-String "Source:*")[1].Line
+                }
+                $timeObjectArray += $timeObject
+
+
+            }
+        }
+    }
+
+    end {
+        return $timeArray
+    }
+}
+
+
+$server = Get-ADComputer -Filter {operatingsystem -like "*server*"}
+
+
+
+
+
